953fa62d3e7ee821b71a569ad4896aeb98649628
migrd <72756664+migrd@users.noreply.github.com>
2021-07-29 23:59:44 -0300

    4 novas funcionalidades na listagem de contatos tem o ui para fazer ligacoes, mandar mensagem, excluir contato e editar

--- src/app/servicos/lista-contatos.service.ts
+++ src/app/servicos/lista-contatos.service.ts
@@ -14,4 +14,11 @@ export class ListaContatosService {
     this.lista.push(new Contato(nome, numero));
     this.lista.sort((a, b) => a.nome.localeCompare(b.nome));
   }
+
+  remover(contato: Contato) {
+    const index = this.lista.indexOf(contato);
+    if (index !== -1) {
+      this.lista.splice(index, 1);
+    }
+  }
 }
